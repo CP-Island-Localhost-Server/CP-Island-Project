67b26e23569b23ba5ce1d166f34e9da86325428e
Wednesday Addams <jordanluders@proton.me>
2024-11-11 05:11:44 -0600

    Assets cleanup.

---
+++ Assets/Editor/WhatIsWhere.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "WhatIsWhere")]
+public class WhatIsWhere : ScriptableObject
+{
+    // This will not be used in the game, this is to state what is where for future notations.
+
+    public string Notes;
+
+    public string AvatarColorPath;
+
+    public string ScheduledEventDatesPath;
+
+    public string SceneAudioPath;
+
+    public string HomescreenPrefabsPath;
+
+    public string HalloweenCostumesPath;
+
+}
