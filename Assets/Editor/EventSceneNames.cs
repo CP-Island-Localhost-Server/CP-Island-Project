693346dc09b74fc542515744f6a33cde860c19c8
Wednesday Addams <jordanluders@proton.me>
2024-12-18 06:00:07 -0600

    Upload a list of event names.

---
+++ Assets/Editor/EventSceneNames.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EventSceneNames")]
+public class EventSceneNames : ScriptableObject
+{
+    // This will not be used in the game, this is to state the event names for future notations.
+
+    public string Notes;
+
+    public string WaddleOn2018Town;
+
+    public string WaddleOn2018Boardwalk;
+
+    public string WaddleOn2018Beach;
+
+    public string WaddleOn2018Diving;
+
+    public string WaddleOn2018MtBlizzard;
+
+    public string Halloween2018Town;
+
+    public string Halloween2018Boardwalk;
+
+    public string Halloween2018Beach;
+
+    public string Halloween2018Diving;
+
+    public string Halloween2018MtBlizzard;
+
+    public string Halloween2024Town;
+
+    public string Halloween2024Boardwalk;
+
+    public string Halloween2024Beach;
+
+    public string Halloween2024Diving;
+
+    public string Halloween2024MtBlizzard;
+
+    public string Halloween2025Town;
+
+    public string Halloween2025Boardwalk;
+
+    public string Halloween2025Beach;
+
+    public string Halloween2025Diving;
+
+    public string Halloween2025MtBlizzard;
+
+    public string MedievalBoardwalk;
+
+    public string PirateExpeditionBeach;
+
+    public string PirateExpeditionBoardwalk;
+
+    public string SummerSplashdownTown;
+
+    public string SummerSplashdownBoardwalk;
+
+    public string SummerSplashdownMtBlizzard;
+
+    public string WorldPenguinDay2018Town;
+
+    public string AprilFools2018Town;
+
+    public string AprilFools2018Boardwalk;
+
+    public string Easter2018Town;
+
+    public string Easter2018Beach;
+
+    public string Easter2018Boardwalk;
+
+    public string Easter2018MtBlizzard;
+
+    public string AnniversaryParty2018Boardwalk;
+
+    public string AnniversaryParty2018Town;
+
+    public string RainbowMigrationBoardwalk;
+
+    public string RainbowMigrationMtBlizzard;
+
+    public string RainbowMigrationTown;
+
+    public string Holiday2017Construction;
+
+    public string Holiday2017Boardwalk;
+
+    public string Holiday2017Beach;
+
+    public string Holiday2017Diving;
+
+    public string Holiday2017MtBlizzard;
+
+    public string Holiday2017December6Town;
+
+    public string Holiday2017December9Town;
+
+    public string Holiday2017December12Town;
+
+    public string Holiday2017December19Town;
+
+    public string ValentinesDayTown;
+
+}
